b2aaef409f3a23115e2ca34b2d72dda344811962
Sugar <topevol33@gmail.com>
2024-09-20 17:41:12 +0300

    Refactoring code for video input

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 import os
+import argparse
 import cv2
 import mediapipe as mp
 
-output_dir = './output'
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-
-#read image
-img_path = './data/testImg.png'
-img = cv2.imread(img_path)
-
-H, W, _ = img.shape
-
-#detect faces
-mp_face_detection = mp.solutions.face_detection
-
-with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
+def process_img(img, face_detection):
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
     out = face_detection.process(img_rgb)
 
     if out.detections is not None:
@@ -31,17 +20,54 @@ with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence
             w = int(w * W)
             h = int(h * H)
 
-            #img = cv2.rectangle(img, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 10)
+            # img = cv2.rectangle(img, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 10)
 
-            #blur faces
-            #applying blur on all picture
-            #img = cv2.blur(img, (50, 50))
-            #applying blur only on face
+            # blur faces
+            # applying blur on all picture
+            # img = cv2.blur(img, (50, 50))
+            # applying blur only on face
             img[y1:y1 + h, x1:x1 + w, :] = cv2.blur(img[y1:y1 + h, x1:x1 + w, :], (30, 30))
 
-    # cv2.imshow('img', img)
-    # cv2.waitKey(0)
+    return img
 
-#save image
 
-cv2.imwrite(os.path.join(output_dir, 'output.png'), img)
+args = argparse.ArgumentParser()
+args.add_argument("--mode", default='image')
+args.add_argument("--filePath", default='./data/testImg.png')
+args = args.parse_args()
+
+output_dir = './output'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+#detect faces
+mp_face_detection = mp.solutions.face_detection
+
+with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
+
+    if args.mode in ["image"]:
+        #read image
+        img = cv2.imread(args.filePath)
+        H, W, _ = img.shape
+
+        img = process_img(img, face_detection)
+
+        #save image
+        cv2.imwrite(os.path.join(output_dir, 'output.png'), img)
+
+    elif args.mode in ['video']:
+
+        cap = cv2.VideoCapture(args.filePath)
+        ret, frame = cap.read()
+
+        output_video = cv2.VideoWriter(os.path.join(output_dir, 'output.mp4'),
+                                       cv2.VideoWriter_fourcc(*'MP4V'),
+                                       25,
+                                       (frame.shape[1], frame.shape[0]))
+
+        while True:
+            frame = process_img(frame, face_detection)
+
+            ret, frame = cap.read()
+
+        cap.release()
